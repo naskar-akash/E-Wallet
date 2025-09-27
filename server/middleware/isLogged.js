0668771cd8f3642e8c4ea27eca1cf1a42beaaca4
naskar-akash <naskar.akash1947@gmail.com>
2025-09-27 19:33:13 +0530

    setting middleware

--- server/middleware/isLogged.js
+++ server/middleware/isLogged.js
@@ -1,18 +1,24 @@
 const jwt = require("jsonwebtoken");
 const User = require("../model/user-model");
 
-const isLogged = async(req, res, next) => {
-    if(!req.cookies.token) {
-        res.status(403).json({message: "Login first!"});
+const isLogged = async (req, res, next) => {
+  if (!req.cookies.token) {
+    return res.status(403).json({ message: "Login first!" });
+  }
+  try {
+    const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+    const user = await User.findOne({ email: decoded.email }).select(
+      "-password"
+    );
+    if (!user) {
+      return res.status(401).json({ message: "User not found!" });
     }
-    try {
-        const decoded = jwt.verify(req.cookies.token,process.env.JWT_SECRET);
-        const user = await User.findOne({email: decoded.email}).select("-password");
-        req.user = user;
-        next();
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-}
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid or expired token!" });
+  }
+};
 
 module.exports.isLogged = isLogged;
